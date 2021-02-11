88a71a7b7b4d8e5d5f87146e8e16b551c7da9a82
farhanashiril <farhanashiril04@gmail.com>
2021-02-11 12:04:19 +0530

    pi commit

--- myapp2/urls.py
+++ myapp2/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path,include
 from . import views
 
-urlspatterns=[
-
-path('base',views.function,name="base"),
-
+urlpatterns = [
+   path('base',views.function,name="base"),
 ]
