3a590058626fe1f57776ab1d9070b212713ebb10
farhanashiril <farhanashiril04@gmail.com>
2021-02-11 11:45:52 +0530

    jj commit

---
+++ myapp2/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from . import views
+
+urlspatterns=[
+
+path('base',views.assignment1,name="base"),
+
+]
--- myapp2/views.py
+++ myapp2/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def fun1(request):
+    return render(request,'base.html')
