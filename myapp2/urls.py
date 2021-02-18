b1bfe19a379f2116775e284a2b2b55a4eac3dbb0
farhanashiril <farhanashiril04@gmail.com>
2021-02-18 13:01:12 +0530

    r commit

--- myapp2/urls.py
+++ myapp2/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   path('base',views.function,name="base"),
+   path('mag',views.magni,name="mag"),
 ]
--- myapp2/views.py
+++ myapp2/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def function(request):
-    return render(request,'base.html')
+def magni(request):
+    return render(request,'magnificent.html')
